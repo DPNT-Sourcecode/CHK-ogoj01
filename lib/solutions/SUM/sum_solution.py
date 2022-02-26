cae7d51a32aa696c82ff27d952e73d2760049215
sbx_user1051 <sbx_user1051@169.254.89.13>
2022-02-26 20:47:51 +0000

    Sat Feb 26 20:47:51 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,6 +1,9 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(value1: int, value2: int) -> int:
+class Solution:
+
+    def compute(value1: int, value2: int) -> int:
         if value1 not in [0,100] or value2 not in [0,100]:
             raise Exception("params not in accepted range")
 
         return int(value1 + value2)
+
