36543c495a896e0205c9c99f33576b02ced91c78
sbx_user1051 <sbx_user1051@169.254.67.145>
2022-02-28 20:48:16 +0000

    Mon Feb 28 20:48:16 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,8 +2,8 @@
 # skus = unicode string
 def checkout(skus):
     '''
-    input: string of goods to checkout. example: 'ABCADAZ'
-    output: total checkout value (attention to special offers)
+    Input: string of goods to checkout. Example: 'ABCADAZ'
+    Output: total checkout value (attention to special offers)
     '''
 
     price_rules = [(3, {'A': 130}), (2, {'B': 45}), (1, {'A': 50}), (1, {'B': 30}), (1, {'C': 20}), (1, {'D': 15})]
@@ -14,29 +14,20 @@ def checkout(skus):
         if item not in items_in_store:
             return -1
 
-
     for quantity, item_map in price_rules:
         for item, price in item_map.items():
             
             price_rule = quantity * item
-            print(f"price_rule {price_rule}")
-
-            if price_rule in skus:
-                print(total)
-                print(price_rule)
-                print(skus.count(price_rule))
+           
+            if price_rule in skus:                
                 total += skus.count(price_rule) * price
-                skus = skus.replace(price_rule, '')
-                print(f"skus: {skus}")
-                print(total)
-
-            else:
-                print('not found')
+                skus = skus.replace(price_rule, '')                
 
     return total
 
+'''
 if __name__ == '__main__':
-    '''
+    
     print('Test1...')
     purchase = 'ABCFF'
     print(checkout(purchase))
@@ -75,21 +66,12 @@ if __name__ == '__main__':
     purchase = 'AAAAA'
     print(checkout(purchase))
     print(130+50+50)
-    '''
+    
     print('Test9...')
     purchase = 'AAAAABBBCCCD'
     print(checkout(purchase))
-    print(130+50+50)
-
-
-
-
-    
-
-    
-
-
-
+    print(130+50+50+45+30+20+20+20+15)
+'''
 
 
 
