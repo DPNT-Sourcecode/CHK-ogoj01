d783172c922a41a4684d91b82c8639c06321cd25
sbx_user1051 <sbx_user1051@169.254.67.145>
2022-02-28 20:03:16 +0000

    Mon Feb 28 20:03:16 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,11 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    
-    
+    '''
+    input: string of goods to checkout. example: 'ABCADAZ'
+    output: total checkout value (attention to special offers)
+    '''
+
+
+
 
